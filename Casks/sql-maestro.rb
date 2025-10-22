6395423677e963dc07f87b9b9b447d2a30b29ee7
cxj05h <cxj05h@acu.edu>
2025-10-22 16:19:37 +0300

    udpated new brew version v1.7.6

--- Casks/sql-maestro.rb
+++ Casks/sql-maestro.rb
@@ -1,7 +1,7 @@
 cask "sql-maestro" do
-  version "1.7.5"
+  version "1.7.6"
   #revision 1
-  sha256 "a8607e11d21cb5b19833bced15d2e6f06e0a5f246fd93edd77dd7f7f9c667bee"
+  sha256 "13c4007bc1238087e3e75c51cbe50a6bddbc6466993143aebcba6ee9e489424e"
 
   url "https://github.com/cxj05h/SQL-Maestro/releases/download/v#{version}/SQLMaestro-v#{version}.zip"
   name "SQL Maestro"
