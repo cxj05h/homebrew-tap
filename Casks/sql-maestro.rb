45230b560aa5c5984abd69b2660833357ccc948f
cxj05h <cxj05h@acu.edu>
2025-10-27 11:24:38 +0200

    udpated new brew version v1.9.6

--- Casks/sql-maestro.rb
+++ Casks/sql-maestro.rb
@@ -1,9 +1,14 @@
-cask "sql-maestro" do
-  version "1.9.5"
-  #revision 1
-  sha256 "9b26e0f32b3c2c7aaf040d3cac23215256d0af8083250ab367d5548c44fe20a6"
+# In your cask formula file (e.g., Casks/sql-maestro.rb):
 
-  url "https://github.com/cxj05h/SQL-Maestro/releases/download/v#{version}/SQLMaestro-v#{version}.zip"
+require_relative "../lib/github_private_repo_strategy"
+
+cask "sql-maestro" do
+  version "1.9.6"
+  sha256 "8a62084b07235a8ede053c27a49f68efb1bf5433e1494ab893b389f4a20e232e"
+  
+  url "https://github.com/cxj05h/SQL-Maestro/releases/download/v#{version}/SQLMaestro-v#{version}.zip",
+      using: GitHubPrivateRepositoryReleaseDownloadStrategy
+
   name "SQL Maestro"
   desc "SQL template manager for database troubleshooting"
   homepage "https://github.com/cxj05h/SQL-Maestro"
@@ -19,5 +24,4 @@ cask "sql-maestro" do
     "~/Library/Application Support/SQLMaestro",
     "~/Library/Preferences/com.sqlmaestro.SQLMaestro.plist",
   ]
-end 
-
+end
