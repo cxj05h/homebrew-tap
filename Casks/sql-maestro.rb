d6200200c24742e06c93f7ab3169eb05e356f3ea
cxj05h <cxj05h@acu.edu>
2025-10-27 14:01:05 +0200

    udpated new brew version v1.9.8

--- Casks/sql-maestro.rb
+++ Casks/sql-maestro.rb
@@ -1,8 +1,8 @@
 require_relative "../lib/github_private_repo_strategy"
 
 cask "sql-maestro" do
-  version "1.9.7"
-  sha256 "68cfa3d1648ceaa3702b1ca7d43bf9cab6841de0644e3e60a437c163af0ea05f"
+  version "1.9.8"
+  sha256 "edf33ab2da85e6de06bb3cea0867ea627afa47cf2e2b5c9cdd95007cd9214bd4"
   url "v#{version}", using: GitHubPrivateRepositoryReleaseDownloadStrategy
   name "SQL Maestro"
   desc "SQL template manager for database troubleshooting"
