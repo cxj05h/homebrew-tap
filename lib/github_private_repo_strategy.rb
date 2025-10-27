78cfe76e56884100f5eb07b19ab0f843a9a73e51
cxj05h <cxj05h@acu.edu>
2025-10-27 12:04:08 +0200

    udpated new brew version v1.9.7

--- lib/github_private_repo_strategy.rb
+++ lib/github_private_repo_strategy.rb
@@ -1,15 +1,20 @@
 require "download_strategy"
 
 class GitHubPrivateRepositoryReleaseDownloadStrategy < CurlDownloadStrategy
-  def _headers
+  def fetch(timeout: nil)
+    ohai "Downloading with GitHubPrivateRepositoryReleaseDownloadStrategy"
     raise "HOMEBREW_GITHUB_API_TOKEN is not set!" unless ENV["HOMEBREW_GITHUB_API_TOKEN"]
-    {
-      "Authorization" => "Bearer #{ENV['HOMEBREW_GITHUB_API_TOKEN']}",
-      "Accept" => "application/octet-stream"
-    }
-  end
 
-  def curl(*args, **options)
-    super(*args, **options.merge(headers: _headers))
+    curl_args = [
+      "--location",
+      "--header", "Authorization: Bearer #{ENV['HOMEBREW_GITHUB_API_TOKEN']}",
+      "--header", "Accept: application/octet-stream",
+      "--output", temporary_path,
+      url
+    ]
+    system "curl", *curl_args
+    unless File.exist?(temporary_path)
+      raise "Download failed!"
+    end
   end
 end
